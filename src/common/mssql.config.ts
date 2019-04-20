59f2edfa2ca0cd3c5fd36a06981a553c0c9cdfe0
Mateus Garcia <mateus.gigainfo@gmail.com>
2019-04-20 20:52:55 -0300

    feat(itinerarios,viagens,pontos): atualizado para armazenar mais informações

--- src/common/mssql.config.ts
+++ src/common/mssql.config.ts
@@ -1,8 +1,8 @@
-export const mssqlUser: string = process.env.TRANSCOLDB_USER;
-export const mssqlPassword: string = process.env.TRANSCOLDB_PASSWORD;
-export const mssqlHost: string = process.env.TRANSCOLDB_HOST;
-export const mssqlSchema: string = process.env.TRANSCOLDB_SCHEMA;
-export const mssqlPort: number = Number( process.env.TRANSCOLDB_PORT );
+export const mssqlUser: string = process.env.MINER_TRANSCOLDB_USER;
+export const mssqlPassword: string = process.env.MINER_TRANSCOLDB_PASSWORD;
+export const mssqlHost: string = process.env.MINER_TRANSCOLDB_HOST;
+export const mssqlSchema: string = process.env.MINER_TRANSCOLDB_SCHEMA;
+export const mssqlPort: number = Number( process.env.MINER_TRANSCOLDB_PORT );
 
 
 export const mssqlConnectionString: string =
--- src/main.ts
+++ src/main.ts
@@ -33,23 +33,77 @@ async function main () {
             //console.log( msgToRabbit );
             //publishChannel.sendToQueue( rabbitConf.rabbitPublishQueueName, new Buffer( JSON.stringify( msgToRabbit ) ), { persistent: false } );
 
-            let q1 = msgToRabbit.veiculo.ROTULO;
-            let q2 = msgToRabbit.veiculo.DATAHORA;
-            let q3 = new Date( veiculo.DATAHORA ).toISOString();
-            let q4 = msgToRabbit.veiculo.VELOCIDADE;
-            let q5 = msgToRabbit.veiculo.IGNICAO;
-            let q6 = msgToRabbit.pontosProximos[ 0 ];
 
-            let statement = `INSERT INTO VeiculoXPontos ` +
-                `(veiculo, datahoraMillis, datahora, velocidade, ignicao, ponto_id_geocontrol) VALUES ` +
-                `( '${q1}', ${q2}, '${q3}', ${q4}, '${q5}', ${q6} )`;
+            let rot: string = msgToRabbit.veiculo.ROTULO;
+            let hrchegada: Date = new Date( veiculo.DATAHORA );
+            hrchegada.setUTCHours( hrchegada.getUTCHours() + 3 );
+            let chegada = hrchegada.toISOString();
+            let hrsaida: Date = new Date( veiculo.DATAHORA );
+            hrsaida.setUTCHours( hrsaida.getUTCHours() - 3 );
+            let saida = hrsaida.toISOString();
+
+            let statement1 = `SELECT viagem.id, viagem.itinerario_id FROM viagem `
+                + `INNER JOIN itinerario ON viagem.itinerario_id = itinerario.id `
+                + `where veiculo = '${rot}' and horadachegada < '${chegada}' `
+                + `and horadasaida BETWEEN '${saida}' and '${new Date( veiculo.DATAHORA ).toISOString()}' `;
+            //console.log( statement1 )
             try {
-                await sql.query( statement );
+
+                let result1 = await sql.query( statement1 );
+                if ( result1.recordset.length > 0 ) {
+                    let viagemId = result1.recordset[ 0 ].id;
+                    let itinerarioId = result1.recordset[ 0 ].itinerario_id
+                    let pontoId = msgToRabbit.pontosProximos[ 0 ];
+
+                    let statement2 = `SELECT ponto_id, ordem FROM itinerario_ponto `
+                        + `WHERE itinerario_id = ${itinerarioId} ORDER BY ordem`;
+                    try {
+                        let result2 = await sql.query( statement2 );
+                        let inicial: boolean = false;
+                        let final: boolean = false;
+                        if ( result2.recordset.length > 0 ) {
+                            if ( pontoId == result2.recordset[ 0 ].ponto_id ) {
+                                inicial = true;
+                            }
+                            if ( pontoId == result2.recordset[ result2.recordset.length - 1 ].ponto_id ) {
+                                final = true;
+                            }
+                            let q2 = msgToRabbit.veiculo.DATAHORA;
+                            let q3 = new Date( veiculo.DATAHORA ).toISOString();
+                            let q4 = msgToRabbit.veiculo.VELOCIDADE;
+                            let q5 = msgToRabbit.veiculo.IGNICAO;
+
+                            let statement3 = `INSERT INTO VeiculoXPontos `
+                                + `(veiculo, datahoraMillis, datahora, velocidade, ignicao, ponto_id, `
+                                + `itinerario_id, viagem_id, pontoInicial, pontoFinal) `
+                                + `VALUES ( '${rot}', ${q2}, '${q3}', ${q4}, '${q5}', ${pontoId}, `
+                                + `${itinerarioId}, ${viagemId}, '${inicial}', '${final}' )`;
+
+                            try {
+                                await sql.query( statement3 );
+                            } catch ( err ) {
+                                console.log( '-------------------------------------------' )
+                                let msg = `Erro ao salvar um historico no banco estático\n${err.message}`;
+                                console.log( msg );
+                                console.log( `Query3: ${statement3}` );
+                                console.log( '-------------------------------------------' )
+                            }
+                        }
+                    } catch ( err ) {
+                        console.log( '-------------------------------------------' )
+                        console.log( `Erro ao buscar pontos do itinerario ${itinerarioId}.` );
+                        console.log( err.message );
+                        console.log( `Query2: ${statement2} ` )
+                        console.log( '-------------------------------------------' )
+                    }
+                } else {
+                    //console.log( `Itinerario e viagem para o veiculo ${ rot } não encontrados` )
+                }
             } catch ( err ) {
-                let msg = `Erro ao salvar um historico no banco estático\n${err.message}`;
+                let msg = `Erro ao buscar viagens no banco estático\n${err.message}
+                                query1: ${ statement1} `;
                 console.log( msg );
             }
-
 
         }
         consumerChannel.ack( msg );
