8a92bfd0b528fb95c1ab8061ea2ad67758772144
Mateus Garcia <mateus.gigainfo@gmail.com>
2019-04-22 18:48:46 -0300

    fix(salvaHistoria): update do nome novo da tabela

--- src/services/mssql/salvaHistoria.service.ts
+++ src/services/mssql/salvaHistoria.service.ts
@@ -9,7 +9,7 @@ import { VeiculoXPonto } from '../../DTOs/VeiculoXPonto.interface';
 export async function salvaHistoria ( pool: ConnectionPool, dados: VeiculoXPonto ): Promise<any> {
 
     try {
-        let insertQuery = `INSERT INTO VeiculoXPontos `
+        let insertQuery = `INSERT INTO veiculo_ponto_viagem_historico_bruto `
             + `(veiculo, datahoraMillis, datahora, velocidade, ignicao, ponto_id, `
             + `itinerario_id, viagem_id, pontoInicial, pontoFinal, sequencia) `
             + `VALUES ( '${dados.rotulo}', ${dados.datahoraMillis}, '${dados.datahoraLegivel}', `
